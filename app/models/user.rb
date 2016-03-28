e1251673fb38d049cc82b46044fe90f83f8686bf
johnny-hsiao <johnnyhsiao87@gmail.com>
2016-03-28 21:22:00 +0000

    generating users controller and users/new view

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ActiveRecord::Base
   has_secure_password
+
+  validates :email, presence: true
+  validates :firstname, presence: true
+  validates :lastname, presence: true
+  validates :password, presence: true, length: { in: 6..20 }, on: :create
+  validates :password_confirmation, presence: true, length: { in: 6..20 }, on: :create
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 RottenMangoes::Application.routes.draw do
   resources :movies
+  resources :users, only: [:new, :create]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
