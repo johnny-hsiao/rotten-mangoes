2faf2c3f5bbc80e479a932e66d8e36d391b73db1
johnny-hsiao <johnnyhsiao87@gmail.com>
2016-03-28 20:31:05 +0000

    generating movies controller and views

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,51 @@
+class MoviesController < ApplicationController
+  def index
+    @movies = Movie.all
+  end
+
+  def show
+    @movie = Movie.find_by(id: params[:id])
+  end
+
+  def new
+    @movie = Movie.new
+  end
+
+  def edit
+    @movie = Movie.find_by(id: params[:id])
+  end
+
+  def create
+    @movie = Movie.new(movie_params)
+
+    if @movie.save
+      redirect_to movies_path
+    else
+      render :new
+    end
+  end
+
+  def update
+    @movie = Movie.find_by(id: params[:id])
+
+    if @movie.update_attributes(movie_params)
+      redirect_to @movie
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @movie = Movie.find_by(id: params[:id])
+    @movie.destroy
+    redirect_to movies_path
+  end
+
+  protected
+
+  def movie_params
+    params.require(:movie).permit(
+      :title, :release_date, :director, :runtime_in_minutes, :poster_image_url, :description
+    )
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 RottenMangoes::Application.routes.draw do
+  resources :movies
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
