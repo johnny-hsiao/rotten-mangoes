471d78ea818b80dfaf41c0986e391e6f19671326
johnny-hsiao <johnnyhsiao87@gmail.com>
2016-03-28 22:29:50 +0000

    adding links to log in, signup, or logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -13,4 +13,9 @@ class SessionsController < ApplicationController
       render :new
     end
   end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to movies_path, notice: "Adios!"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ RottenMangoes::Application.routes.draw do
 
   resources :movies
   resources :users, only: [:new, :create]
-  resources :sessions, only: [:new, :create]
+  resources :sessions, only: [:new, :create, :destroy]
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
