76ad586c5bc083adeb7f10fea3cd85c2b5a7bdae
Johnny Hsiao <johnnyhsiao87@gmail.com>
2016-03-30 19:08:12 -0400

    finish feature sessions resource

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ RottenMangoes::Application.routes.draw do
     resources :reviews, only: [:new, :create]
   end
   resources :users, only: [:new, :create]
-  resources :sessions, only: [:new, :create, :destroy]
+  resource :session, only: [:new, :create, :destroy]
   root to: 'movies#index'
 
   namespace :admin do
