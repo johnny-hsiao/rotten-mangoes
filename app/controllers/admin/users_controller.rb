26a31c23e30af43a9323567f37ce54a0dcdbcb2e
johnny-hsiao <johnnyhsiao87@gmail.com>
2016-03-30 17:15:12 +0000

    Finish admin features

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -9,6 +9,34 @@ class Admin::UsersController < Admin::ApplicationController
     @reviews = @user.reviews
   end
 
+  def edit
+    user_selected
+  end
+
+  def update
+    user_selected
+    @user.update_attributes(user_params)
+    if @user.save
+      redirect_to admin_users_path, notice: "User account updated successfully"
+    else
+      render :edit
+    end
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    user = User.new(user_params)
+    if user.save!
+      
+      redirect_to admin_users_path, notice: "User successfully created!"
+    else
+      render :new
+    end
+  end
+
   def destroy
     user_selected
     @user.destroy
@@ -19,6 +47,10 @@ class Admin::UsersController < Admin::ApplicationController
 
   def user_selected
     @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:firstname, :lastname, :email, :password, :password_confirmation, :is_admin)
   end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,6 +17,6 @@ class UsersController < ApplicationController
   protected
 
   def user_params
-    params.require(:user).permit(:email, :firstname, :lastname, :password, :password_confirmation)
+    params.require(:user).permit(:email, :firstname, :lastname, :password, :password_confirmation, :is_admin)
   end
 end
